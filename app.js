72ff7a8157bd05dae3df3895ea3259be5ed5fb23
GogumaYummy <sora1211x@gmail.com>
2022-11-03 12:09:09 +0900

    라우터 구조 수정

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
-const indexRouter = require('./routes/index');
+const pageRouter = require('./routes/pages');
 
 const app = express();
 
@@ -18,7 +18,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+app.use('/', pageRouter);
 
 app.use(function (req, res, next) {
   next(createError(404));
