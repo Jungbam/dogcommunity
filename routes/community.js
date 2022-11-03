1b8838d90be60ede06cd7d7bc2efcad8a2137d4c
Jungbam <108196588+Jungbam@users.noreply.github.com>
2022-11-04 04:46:30 +0900

    상세 페이지 작업, community.js 테스트

--- routes/community.js
+++ routes/community.js
@@ -8,9 +8,40 @@ router.get('/', async (req, res, next) => {
   try {
     const cursor = await db.collection('community').find({});
     console.log(cursor);
-    const articles = await cursor.toArray();
+    // const articles = await cursor.toArray();
 
+    const articles = [
+      {
+        title: '하이',
+        contact: '010-1234-5678',
+        region: '서울',
+        missingDate: Date().now,
+        content: '테스트입니다.',
+      },
+      {
+        title: '테스트2',
+        contact: '010-1234-5678',
+        region: '경기',
+        missingDate: Date().now,
+        content: '테스트2입니다.',
+      },
+    ];
     res.render('community', { articles });
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get('/detail/1', async (req, res, next) => {
+  try {
+    const article = {
+      title: '하이',
+      contact: '010-1234-5678',
+      region: '서울',
+      missingDate: Date().now,
+      content: '테스트입니다.',
+    };
+    res.render('article-detail', { article });
   } catch (err) {
     next(err);
   }
