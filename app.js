93443f5c97fb11c50eabbc2c88b567d5604b61fc
GogumaYummy <sora1211x@gmail.com>
2022-11-04 16:22:13 +0900

    로거 예제 작성

--- app.js
+++ app.js
@@ -13,6 +13,18 @@ app.set('port', process.env.PORT || 5000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+// 로거 예제
+// 위에서부터 높은 레벨
+
+logger.error('에러');
+logger.warn('주의');
+logger.info('정보');
+logger.http('HTTP');
+logger.debug('디버그');
+
+// const logger = require('./config/winston'); 하신 뒤에
+// console.log/console.error 대용으로 logger.info/logger.error 사용하면 됩니다.
+
 app.use(
   morgan(
     // 개발 환경과 배포 환경의 출력 내용 차별화
