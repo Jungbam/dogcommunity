d11900055dd1abf8416ce0124f6d48d1ff9bf417
GogumaYummy <sora1211x@gmail.com>
2022-11-04 14:20:41 +0900

    코드 최적화

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,7 @@
 const { MongoClient } = require('mongodb');
 
 const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
+
 const mongoURI = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(mongoURI);
