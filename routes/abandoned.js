2cc0531abcc652af05777bfa2290c0737dacd3d5
GogumaYummy <sora1211x@gmail.com>
2022-11-04 18:57:31 +0900

    라우터 구조 변경

--- routes/abandoned.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-const router = express.Router();
-
-router.get('/', (req, res, next) => {
-  res.render('abandoned', { title: '길 잃은 동물들의 이야기' });
-});
-
-module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -3,14 +3,12 @@ const express = require('express');
 const router = express.Router();
 
 const mainRouter = require('./main');
-const missingRouter = require('./missing');
-const abandonedRouter = require('./abandoned');
-const communityRouter = require('./community');
+const boardRouter = require('./board');
+const articlesRouter = require('./articles');
 
-// 각 페이지별 요청 처리 js로 연결해줌.
+// 각 라우터별 요청 처리 js로 연결해줌.
 router.use('/', mainRouter);
-router.use('/missing', missingRouter);
-router.use('/abandoned', abandonedRouter);
-router.use('/community', communityRouter);
+router.use('/board', boardRouter);
+router.use('/articles', articlesRouter);
 
 module.exports = router;
