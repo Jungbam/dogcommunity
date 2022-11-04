d09174e76c56c65d0316419251f09d1a468f3c32
GogumaYummy <sora1211x@gmail.com>
2022-11-04 13:34:18 +0900

    dotenv를 명령줄에서 로드하도록 변경, 환경에 따른 로깅 설정

--- app.js
+++ app.js
@@ -3,18 +3,19 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const morgan = require('morgan'); // HTTP 요청 로그에 대한 패키지
-const dotenv = require('dotenv');
 
 const indexRouter = require('./routes/index');
 
-dotenv.config(); // 환경변수를 불러오기 위해 사용
 const app = express();
+const env = process.env.ENV;
+
+console.log(env);
 
 app.set('port', process.env.PORT || 5000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(morgan('dev'));
+app.use(morgan(env === 'development' ? 'dev' : 'combined'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -28,7 +29,7 @@ app.use((req, res, next) => {
 
 app.use((err, req, res, next) => {
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = env === 'development' ? err : {};
 
   res.status(err.status || 500);
   res.render('error');
--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,4 @@
-const dotenv = require('dotenv');
 const { MongoClient } = require('mongodb');
-
-dotenv.config();
 
 const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
 const mongoURI = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/?retryWrites=true&w=majority`;
